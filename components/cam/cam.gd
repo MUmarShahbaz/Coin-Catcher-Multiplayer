34546bba4fbce887bc1ddc35772af7c77581e0ac
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-10-26 00:29:03 +0500

    Made a Camera

---
+++ components/cam/cam.gd
@@ -0,0 +1,22 @@
+extends Camera2D
+class_name CAM
+
+@onready var target: CharacterBody2D = get_parent()
+@export var smoothing: float = 0.1
+@export var offset_distance: Vector2 = Vector2(120, 60)
+var target_offset: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	zoom = Vector2.ONE * 3.5
+
+func _process(_delta: float) -> void:
+	var cam_off: Vector2 = Vector2.ZERO
+	cam_off.x = Input.get_axis("cam_left", "cam_right")
+	cam_off.y = Input.get_axis("cam_up", "cam_down")
+	set_target_offset(cam_off)
+	if target:
+		global_position = lerp(global_position, target.global_position + target_offset, smoothing)
+
+func set_target_offset(new_offset: Vector2):
+	target_offset.x = new_offset.x * offset_distance.x
+	target_offset.y = new_offset.y * offset_distance.y
