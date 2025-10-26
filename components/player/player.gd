6f01a781f9f6e0a6309a6d07f28ff81464832af3
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-10-26 19:06:03 +0500

    Modified Player and Map

--- components/player/player.gd
+++ components/player/player.gd
@@ -5,6 +5,7 @@ class_name Player
 @export var JUMP_VELOCITY = -300.0
 @onready var sprite: AnimatedSprite2D = $sprite
 @onready var jump_sfx: AudioStreamPlayer = $jump
+@onready var die_sfx: AudioStreamPlayer = $die
 
 var facing: int = 1
 
@@ -12,3 +13,8 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	move_and_slide()
+
+func kill():
+	global_position = Vector2(0, -80)
+	velocity = Vector2.ZERO
+	die_sfx.play()
