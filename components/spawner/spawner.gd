abd5bd5469df5d55dd495ffa5fa183645e1b13b7
M. Umar Shahbaz <m.umarshahbaz.2007@gmail.com>
2025-10-27 04:15:01 +0500

    Final minor edits
    Still need to get a server for multiplayer

--- components/spawner/spawner.gd
+++ components/spawner/spawner.gd
@@ -3,6 +3,7 @@ class_name Spawner
 
 @export var element: PackedScene
 @onready var collider: CollisionShape2D = $spawn_area
+@export var root: Node
 @export_range(0, 2) var base_drop_time: float = 1
 
 var t:= 0.0
@@ -21,5 +22,8 @@ func spawn():
 	var local_pos = Vector2(rand_x, rand_y)
 	var new_element: Node = element.instantiate()
 	new_element.global_position = local_pos + global_position
-	add_sibling(new_element)
+	if root == null:
+		add_sibling(new_element)
+	else:
+		root.add_child(new_element)
 	return new_element
